ba78dae285e7f6bba0a9ce3f010c3c20a214d39c
Clément Moyroud <clement.moyroud@gmail.com>
2016-12-18 18:36:51 -0800

    More useful logging of errors.

--- lib/quiz_request_sink.dart
+++ lib/quiz_request_sink.dart
@@ -14,7 +14,7 @@ class QuizRequestSink extends RequestSink {
     var db = config.database;
     var persistentStore = new PostgreSQLPersistentStore.fromConnectionInfo(db.username, db.password, db.host, db.port, db.databaseName);
 
-    logger.onRecord.listen((rec) => print("${rec.level.name}: ${rec.time}: ${rec.message}"));
+    logger.onRecord.listen((rec) => print("${rec.level.name}: ${rec.time}: ${rec.message} ::: ${rec.error.toString()}"));
 
     context = new ManagedContext(dataModel, persistentStore);
   }
