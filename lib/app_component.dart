5b32058f203e05a728d7d0f69f36ed42f8c3b3ad
Clément Moyroud <clement.moyroud@gmail.com>
2016-12-19 18:29:50 -0800

    Load questions as objects from Aqueduct to Angular.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,5 +1,7 @@
 import 'package:angular2/core.dart';
 import 'package:path/path.dart' as path;
+import 'dart:html';
+import 'dart:convert';
 
 // Every Angular application has at least one component: the root component, named AppComponent here.
 // Components are the basic building blocks of Angular applications. A component controls a portion of the screen—a view—through its associated template.
@@ -8,6 +10,11 @@ import 'package:path/path.dart' as path;
     templateUrl: "app_component.html")
 class AppComponent {
   static const String baseURL = "http://127.0.0.1:8080/api";
+  List questions;
+
+  AppComponent() {
+    loadData();
+  }
 
   // Based on https://webdev.dartlang.org/articles/get-data/json-web-service
   void loadData() {
@@ -18,7 +25,25 @@ class AppComponent {
   }
 
   void onDataLoaded(String responseText) {
-    var jsonString = responseText;
-    print(jsonString);
+    var dataFromJson = JSON.decode(responseText);
+
+    questions = new List<Question>();
+    for (var obj in dataFromJson) {
+      questions.add(new Question.fromJSON(obj));
+    }
   }
 }
+
+class Question {
+  int id;
+  String description;
+  String answer;
+
+  Question(this.id, this.description, this.answer);
+  factory Question.fromJSON(Map<String, dynamic> qst) =>
+    new Question(_toInt(qst['index']), qst['description'], qst['answer']);
+  
+  Map toJSON() => {'index': id, 'description': description, 'answer': answer};
+}
+
+int _toInt(id) => id is int ? id : int.parse(id);
