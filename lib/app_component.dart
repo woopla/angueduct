f6a6e17cc12c8d5f3711b6b93dd8ef541bb2dce9
Clément Moyroud <clement.moyroud@gmail.com>
2016-12-24 14:52:49 -0800

    Correct issue with remote API port.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -8,9 +8,8 @@ import 'dart:convert';
 @Component(
     selector: 'my-app',
     templateUrl: "app_component.html")
-  static const String baseURL = "http://127.0.0.1:8080/api";
 class AppComponent implements OnInit {
-  static const String baseURL = "http://127.0.0.1:8081/api";
+  static const String baseURL = "http://127.0.0.1:8080/api";
   String title = "Quiz time!";
   List<Question> _questions;
 
