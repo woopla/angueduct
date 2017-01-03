00f0670aa542862636401f17d74ff31de68b6e35
Clément Moyroud <clement.moyroud@gmail.com>
2017-01-02 16:03:39 -0800

    Implement by-column sorting in the table. Crude design, but the underlying logic works.

--- lib/table_component.dart
+++ lib/table_component.dart
@@ -10,6 +10,26 @@ class TableComponent {
   @Input()
   bool showIDs = false;
 
+  List<bool> incrOrdering;
+
+  void onClick(String column) {
+    if (incrOrdering == null) {
+      incrOrdering =
+          new List<bool>.filled(columnNames.length, true, growable: true);
+    }
+    int idx = columnNames.indexOf(column);
+    // TODO: Figure out why the table is not updated in the DOM - the sorting works fine...
+    if (incrOrdering[idx]) {
+      incrOrdering[idx] = false;
+      tableData
+          .sort((List<dynamic> a, List<dynamic> b) => a[idx].compareTo(b[idx]));
+    } else {
+      incrOrdering[idx] = true;
+      tableData
+          .sort((List<dynamic> a, List<dynamic> b) => b[idx].compareTo(a[idx]));
+    }
+  }
+
   // Used by ngForTrackBy in the HTML template code
   int trackByRow(int index, List<dynamic> row) => row[0];
 }
